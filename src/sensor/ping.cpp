bef8e47e57f291f0bb35e4bed765d47c6e8966fa
Patrick José Pereira <patrickelectric@gmail.com>
2019-12-16 13:36:25 -0300

    ping: Move from QThread::msleep to QTimer::singleShot
    This approach will fix interface hiccups while flashing
    avoiding the sleep of the main thread
    
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/sensor/ping.cpp
+++ src/sensor/ping.cpp
@@ -359,25 +359,33 @@ void Ping::flash(const QString& fileUrl, bool sendPingGotoBootloader, int baud, 
 
     qCDebug(PING_PROTOCOL_PING) << "Finish connection.";
     // TODO: Move thread delay to something more.. correct.
-    QThread::msleep(1000);
-    link()->finishConnection();
+    auto flashSensor = std::function<void(void)>();
+    auto finishConnection = std::function<void(void)>();
 
-    QSerialPortInfo pInfo(serialLink->port()->portName());
-    QString portLocation = pInfo.systemLocation();
+    flashSensor = [=] {
+        flasher()->setBaudRate(baud);
+        flasher()->setFirmwarePath(fileUrl);
+        flasher()->setLink(link()->configuration()[0]);
+        flasher()->setVerify(verify);
+        flasher()->flash();
+    };
 
-    qCDebug(PING_PROTOCOL_PING) << "Save sensor configuration.";
-    updatePingConfigurationSettings();
+    finishConnection = [=] {
+        QThread::msleep(1000);
+        link()->finishConnection();
 
-    qCDebug(PING_PROTOCOL_PING) << "Start flash.";
-    QThread::msleep(1000);
+        QSerialPortInfo pInfo(serialLink->port()->portName());
+        QString portLocation = pInfo.systemLocation();
 
-    flasher()->setBaudRate(baud);
-    flasher()->setFirmwarePath(fileUrl);
-    flasher()->setLink(link()->configuration()[0]);
-    flasher()->setVerify(verify);
-    flasher()->flash();
+        qCDebug(PING_PROTOCOL_PING) << "Save sensor configuration.";
+        updatePingConfigurationSettings();
 
-    QThread::msleep(500);
+        qCDebug(PING_PROTOCOL_PING) << "Start flash.";
+
+        QTimer::singleShot(1000, flashSensor);
+    };
+    QTimer::singleShot(1000, finishConnection);
+
     // Clear last configuration src ID to detect device as a new one
     connect(&_flasher, &Flasher::stateChanged, this, [this] {
         if (flasher()->state() == Flasher::States::FlashFinished) {
